b2ad099ebeaebd2a068c0e21938ebc79968e6c45
Aaron Karpie <aaronkarpie@gmail.com>
2021-04-09 13:09:21 -0700

    scaling axes

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -21,3 +21,42 @@ var svg = d3
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight)
+
+// Append an SVG group 
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+
+var chosenXAxis = "Poverty"
+
+// function used for updating x-scale var upon click on axis label
+function xScale(stateData, chosenXAxis){
+    var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenXAxis]) * 0.8, 
+    d3.max(stateData, d=>d[chosenXAxis]) * 1.2
+])
+.range([0,width]);
+
+return xLinearScale
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale); 
+
+    xAxis.transition()
+         .duration(1000)
+         .call(bottomAxis)
+    return xAxis
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale,chosenXAxis){
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXAxis]));
+    
+    return circlesGroup
+}
