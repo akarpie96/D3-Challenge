07b1adad137794b309dc9b5692556ceb93bb13d1
Aaron Karpie <aaronkarpie@gmail.com>
2021-04-09 14:38:06 -0700

    adding labels group

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -92,3 +92,68 @@ function updateToolTip(chosenXAxis, circlesGroup){
     })
 return circlesGroup;
 }
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("data/data.csv").then(function(stateData,err){
+    if (err) throw err; 
+
+  // parse data
+
+stateData.forEach(function(data){
+    data.poverty = +data.poverty
+    data.age = +data.age
+    data.income = +data.income
+    data.obesity = +data.obesity
+})
+
+// xLinearScale function above csv import
+var xLinearScale = xScale(stateData, chosenXAxis);
+
+// Create y scale function 
+
+var yLinearScale = d3.scaleLinear()
+    .domain([0,d3.max(hairData, d=> d.obesity)])
+    .range([height,0])
+
+// Create initial axis functions
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+//append x axis
+
+var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0,${height})`)
+    .call(bottomAxis)
+
+//append y axis 
+
+chartGroup.append("g")
+    .call(leftAxis)
+
+//append initial circles
+
+var circlesGroup = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d=> yLinearScale(d.obesity))
+    .attr("r", 20)
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+// Create group for three x-axis labels
+var labelsGroup = chartGroup.append("g")
+.attr("transform", `translate(${width/2}, ${height + 20} )`);
+
+var povertyLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "poverty")
+    .classed("active", true)
+    .text("Poverty")
+
+var ageLabel = labelsGroup.append
+ 
+})
